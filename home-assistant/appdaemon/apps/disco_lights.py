3c15cfc152a7baa9ecc81f34834b7c7a13b66ec1
Gerardo Parrello <gparrellobrasca@glovoapp.com>
2022-09-15 01:04:10 +0200

    set random color around chosen value

--- home-assistant/appdaemon/apps/disco_lights.py
+++ home-assistant/appdaemon/apps/disco_lights.py
@@ -6,7 +6,7 @@ class DiscoLights(hass.Hass):
     def initialize(self):
         self.switch = self.args['switch']
         self.lights = self.args['lights']
-        self.color = self.random_color()
+        self.color = [255,255,255]
         self.listen_state(self.start, self.switch, new = 'on')
         self.listen_state(self.set_color, self.switch, attribute = 'rgb_color')
 
@@ -23,21 +23,16 @@ class DiscoLights(hass.Hass):
                     light = light
                 )
 
-    @staticmethod
-    def random_color():
-        # playing with reds
-        blue = choice((0,randrange(255)))
-        green = randrange(255) if blue == 0 else 0
-        red = 255
-        return (red, green, blue)
-
     def set_color(self, entity, attribute, old, new, kwargs):
-        self.log(new)
         self.color = new
+
+    def random_color(self):
+        predominant = max(self.color)
+        return [predominant if rgb == predominant else randrange(predominant) for rgb in self.color]
 
     def loop_light(self, kwargs):
         light = kwargs['light']
-        self.toggle(light, rgb_color=self.color, brightness=255)
+        self.toggle(light, rgb_color=self.random_color(), brightness=255)
         if self.is_enabled():
             self.run_in(
                 self.loop_light,
